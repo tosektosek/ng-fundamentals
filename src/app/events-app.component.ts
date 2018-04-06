96d97e8ec9f7f58925839af36d0a1ace8d053870
tosektosek <tosektosek@gmail.com>
2018-04-06 22:12:00 +0200

    Initial commit

---
+++ src/app/events-app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-events',
+  template:  `
+  <h2>Hello World</h2>
+  <img src= "/assets/images/basic-shield.png" />
+  `
+})
+export class EventsAppComponent {
+  title = 'app';
+}
